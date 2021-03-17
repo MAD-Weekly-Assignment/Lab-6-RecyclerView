d60e762db030f4139d791061994ab88cc4259f35
AshishManandhar9 <aashishmanandhar99@gmail.com>
2021-03-18 00:51:07 +0545

    Created the ViewHolder for the adapter

--- RecyclerView/app/src/main/java/com/example/recyclerview/WordListAdapter.java
+++ RecyclerView/app/src/main/java/com/example/recyclerview/WordListAdapter.java
@@ -1,6 +1,8 @@
 package com.example.recyclerview;
 
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -21,5 +23,17 @@ public class WordListAdapter extends
     @Override
     public int getItemCount() {
         return 0;
+    }
+
+    class WordViewHolder extends RecyclerView.ViewHolder {
+        public final TextView wordItemView;
+        final WordListAdapter mAdapter;
+
+        public WordViewHolder(View itemView, WordListAdapter adapter) {
+            super(itemView);
+            wordItemView = itemView.findViewById(R.id.word);
+            this.mAdapter = adapter;
+        }
+
     }
 }
