52c940fe78e10a0e8bc5fb5443f0acbb4f0fbaf6
AshishManandhar9 <aashishmanandhar99@gmail.com>
2021-03-18 15:16:22 +0545

    Added a daat provider for recipes

--- RecycleViewHomework/app/src/main/java/com/example/recycleviewhomework/RecipeAdapter.java
+++ RecycleViewHomework/app/src/main/java/com/example/recycleviewhomework/RecipeAdapter.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
 
 
